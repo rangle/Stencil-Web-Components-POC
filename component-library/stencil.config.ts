54212da65ab95960744116957660b819b4985efc
Nathaniel Kessler <nathaniel@nathanielkessler.com>
2019-01-07 16:36:01 -0500

    initial commit

---
+++ component-library/stencil.config.ts
@@ -0,0 +1,13 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'component-library',
+  outputTargets:[
+    { type: 'dist' },
+    { type: 'docs' },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ]
+};
