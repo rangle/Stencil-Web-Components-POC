0016cc726a1b1027b9f601fd2cd027a5f066855f
Nathaniel Kessler <nathaniel@nathanielkessler.com>
2019-01-13 18:21:21 -0500

    stencil-in-angular

---
+++ stencil-in-angular/src/public/stencil/component-library/chunk-cc6147d0.js
@@ -0,0 +1,19 @@
+/*! Built with http://stenciljs.com */
+const { h } = window.ComponentLibrary;
+
+const cssClassesMap = {
+    p: 'padding',
+    m: 'margin',
+    l: 'left',
+    r: 'right'
+};
+const getFirstKey = (obj) => Object.keys(obj).shift();
+const getCssKey = (key) => key.split('').reduce((acc, char) => {
+    return (acc) ? `${acc}-${cssClassesMap[char]}` : `${cssClassesMap[char]}`;
+}, null);
+const mapSpaceStyles = (spaces) => spaces.reduce((acc, space) => {
+    const key = getFirstKey(space);
+    return Object.assign({}, acc, { [getCssKey(key)]: `var(--space-${space[key]})` });
+}, {});
+
+export { mapSpaceStyles as a };
---
+++ stencil-in-angular/src/public/stencil/component-library/cl-box.entry.js
@@ -0,0 +1,26 @@
+/*! Built with http://stenciljs.com */
+const { h } = window.ComponentLibrary;
+
+import { a as mapSpaceStyles } from './chunk-cc6147d0.js';
+
+class ClBox {
+    constructor() {
+        this.spaces = [];
+    }
+    render() {
+        const styles = mapSpaceStyles(this.spaces);
+        return (h("div", { style: Object.assign({}, styles) },
+            h("slot", null)));
+    }
+    static get is() { return "cl-box"; }
+    static get encapsulation() { return "shadow"; }
+    static get properties() { return {
+        "spaces": {
+            "type": "Any",
+            "attr": "spaces"
+        }
+    }; }
+    static get style() { return ""; }
+}
+
+export { ClBox };
