0b2cdfe4866179a5c6bcb0f5996ca4ac0450062c
Nathaniel Kessler <nathaniel@nathanielkessler.com>
2019-01-15 16:05:46 -0500

    storybook wip

--- component-library/src/components/cl-avatar/cl-avatar.stories.js
+++ component-library/src/components/cl-avatar/cl-avatar.stories.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/polymer';
 
 const stories = storiesOf('My component', module);
-stories.add('default', () => (`<cl-avatar></cl-avatar>`));
+stories.add('Avatar', () => (`<cl-avatar src="http://cdn.worldweatheronline.net/images/wsymbols01_png_64/wsymbol_0011_light_snow_showers.png"></cl-avatar>`));
 stories.add('test', () => (`<button>hi</button>`));
